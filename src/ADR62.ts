42efe87c0890b51ad58fd63c3c4371a9c554744f
guidota <guido@decentraland.org>
2022-03-16 13:48:33 -0300

    feat(tests): adding ADR62 tests

--- src/ADR62.ts
+++ src/ADR62.ts
@@ -1,15 +1,16 @@
 import { keccak256 } from "ethereum-cryptography/keccak"
+import { toHex } from "ethereum-cryptography/utils"
 
 /**
  * Calculates the metadata hash. Uses the keys to determine which fields of the metadata object will be used for the result.
  * @public
  */
-function keccak256Hash(metadata: any, keys: string[]): string {
-  const partialMetadata = JSON.stringify(pick(metadata, keys))
+export function keccak256Hash(metadata: any, keys: string[]): string {
+  const partialMetadata = JSON.stringify(pick(metadata, keys.sort()))
   const data = new TextEncoder().encode(partialMetadata)
   const hash = keccak256(data)
-  return new TextDecoder().decode(hash)
+  return toHex(hash)
 }
 
-const pick = (obj: { [x: string]: any }, ...keys: any[]) =>
+const pick = (obj: { [x: string]: any }, keys: string[]) =>
   Object.fromEntries(keys.filter((key) => key in obj).map((key) => [key, obj[key]]))
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from "./node"
 export * from "./ADR32"
+export * from "./ADR62"
